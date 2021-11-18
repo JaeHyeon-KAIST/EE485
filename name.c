d17975e0d331643a550c7c71a2a3defe464d5a46
2 <alvinlog@kaist.ac.kr>
2021-11-18 23:57:53 +0900

    name

---
+++ name.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  assignment_1
+//
+//  Created by JaeHyeon on 2021/09/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
